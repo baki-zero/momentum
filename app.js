53722951ab5a29ed492bbc0c7d7eba569136e5c5
baki-zero <bakizero815@gmail.com>
2023-01-18 00:07:07 +0900

    age calculator

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const calculator = {
-    add : function(a, b) {	//덧셈
-        return a+b;
-    },
-    sub : function(a, b) {	//뺄셈
-        return a-b;
-    },
-    div : function(a, b) {	//나눗셈
-        return a/b;
-    },
-    mul : function(a, b) {	//곱셈
-        return a*b;
-    },
-    squ : function(a, b) {	//제곱
-        return a**b;
-    },
-}
+//나이 계산기
+//이 계산기는 음주가 가능한 나이인지 아닌지를 알려줌
 
-const addResult = calculator.add(1, 1);
-const subResult = calculator.sub(addResult, 0);
-const divResult = calculator.div(20, subResult);
-const mulResult = calculator.mul(divResult, addResult);
-const squResult = calculator.squ(mulResult, subResult);
+const age = parseInt( prompt("How are you?") );
+
+if (isNaN(age) || age < 0 ) {   //age가 숫자가 아니거나 음수일 때 문장 실행
+    console.log("Please write a real positive number");
+} else if( age < 18 ) {         //age가 18미만일 때
+    console.log("You are too young.");
+} else if( age >= 18 && age <= 50 ) {   //age가 18이상이고 50이하일 때
+    console.log("You can drink");
+} else if( age > 50 && age <= 80 ) {    //age가 50 초과이고 80이하일 때
+    console.log("You should excercise");
+} else if(age === 100) {                //age가 100일 때
+    console.log("WOW you are wise");
+} else if(age > 80) {                   //age가 80 초과일 때
+    console.log("You can do whatever you want.");
+} 
